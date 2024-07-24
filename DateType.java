98ec67b318d48d00b9babf61138826f8fa57b0f3
Ariel Mendoza <medifbryan@gmail.com>
2024-07-23 23:40:22 -0500

    hey

---
+++ DateType.java
@@ -0,0 +1,14 @@
+
+
+public  class DateType{
+    public static void main(String[] args){
+
+
+        byte numberPrueba = 123;
+        long numberLon = 1234556L;
+        var number = 10; ;
+        short numberLitter = -2;
+        long n = 3110000000L;
+        
+    }
+}
